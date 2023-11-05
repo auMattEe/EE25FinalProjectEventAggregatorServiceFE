0f1d2bcbc360b48e8b873910ec20a5930f084ffb
auMattEe <matt.eaton85@gmail.com>
2023-11-05 13:59:23 +0200

    Update to login service

--- src/Service/login.service.ts
+++ src/Service/login.service.ts
@@ -10,23 +10,23 @@ import { environment } from '../environments/environment';
 export class LoginService {
   private baseUrl = environment.apiBaseURL;
   constructor(private http: HttpClient) {}
-
   login(username: string, password: string): Observable<any> {
-    // Implement the logic to send a POST request to theSpring Boot API for login.
+    // Implement the logic to send a POST request to the Spring Boot API for login.
     const loginData = { username, password };
-    return this.http.post(`${this.baseUrl}/login`, loginData).pipe(
-      tap((response) => {
-        console.log('Login Response:', response);
-      }),
-      catchError((error) => {
-        console.error('Login Error:', error);
-        throw error; // Re-throw the error to propagate it further
-      })
-    );
+    return this.http.post(`${this.baseUrl}/events/login`, loginData)
+      .pipe(
+        tap((response) => {
+          console.log('Login Response:', response);
+        }),
+        catchError((error) => {
+          console.error('Login Error:', error);
+          throw error; // Re-throw the error to propagate it further
+        })
+      );
   }
 
   register(user: any) {
     // Implement the logic to send a POST request to the Spring Boot API for registration.
-    return this.http.post<any>('/register', user);
+    return this.http.post<any>('/events/register', user);
   }
 }
